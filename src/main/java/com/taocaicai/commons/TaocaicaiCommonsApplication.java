8e26c18025f42deb142e8c359d5b02aa1f791db1
fangchao <fangchao@vandream.com>
2019-01-22 17:34:55 +0800

    first commit

---
+++ src/main/java/com/taocaicai/commons/TaocaicaiCommonsApplication.java
@@ -0,0 +1,14 @@
+package com.taocaicai.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaocaicaiCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaocaicaiCommonsApplication.class, args);
+    }
+
+}
+
